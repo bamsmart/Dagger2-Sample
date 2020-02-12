7db1b861cdbe3fdb5830a433ae66faf8080e26bd
bambang tryono <bamsmart.id@gmail.com>
2020-02-12 11:34:30 +0700

    init

--- app/src/main/java/com/an/dagger/data/remote/api/MovieApiService.java
+++ app/src/main/java/com/an/dagger/data/remote/api/MovieApiService.java
@@ -7,6 +7,6 @@ import retrofit2.http.GET;
 
 public interface MovieApiService {
 
-    @GET("movie/popular?language=en-US&region=US&page=1")
+    @GET("movie/popular?api_key=0aba4f721d8337287b06a15824fe55c4&language=en-US&region=US&page=1")
     Observable<MovieApiResponse> fetchMovies();
 }
